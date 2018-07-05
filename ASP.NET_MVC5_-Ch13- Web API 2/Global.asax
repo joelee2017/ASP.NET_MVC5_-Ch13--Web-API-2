79f4018662c7a32c2c8d4aa291795cb292543800
joe lee <as955133@gmail.com>
2018-07-05 08:14:42 +0800

    加入專案檔案。

---
+++ ASP.NET_MVC5_-Ch13- Web API 2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NET_MVC5__Ch13__Web_API_2.MvcApplication" Language="C#" %>
---
+++ ASP.NET_MVC5_-Ch13- Web API 2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_MVC5__Ch13__Web_API_2.Startup))]
+namespace ASP.NET_MVC5__Ch13__Web_API_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
